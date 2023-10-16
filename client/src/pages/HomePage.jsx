a97ffb5754de4f40d29cc0fe28bce41d13a60d77
Om-chhayala <om20082003@gmail.com>
2023-10-16 17:40:04 +0530

    conditional rendering of create note button

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { CreateNote } from '../components/CreateNote';
+import React, { useEffect } from 'react';
 import { TopBar } from '../components/TopBar';
-import axios from 'axios';
-import '../styles/homepage.css';
-import Notes, { Note } from './Notes';
+import { Notes } from './Notes'; // Assuming 'Notes' is a component
 import { useNavigate } from 'react-router-dom';
 
 export function HomePage() {
+  const checkToken = () => {
+    const token = localStorage.getItem("token");
+    return !!token; // Return true if a token exists
+  };
+
+  useEffect(() => {
+    const isAuthenticated = checkToken();
+    if (!isAuthenticated) {
+      // Redirect to login if the user is not authenticated
+      navigate("/login");
+    }
+  }, []);
+
   const navigate = useNavigate();
+
   return (
     <>
       <TopBar />
-      <Notes/>
+      <Notes />
       <div className="homepage">
-
-            <button className="plus-icon"
-            onClick={() => {
-              navigate("/createnote")
+        <button
+          className="plus-icon"
+          onClick={() => {
+            const isAuthenticated = checkToken();
+            if (isAuthenticated) {
+              navigate("/createnote");
+            } else {
+              navigate("/login");
+            }
           }}
-            >   +
-            
-            </button>
-      
+        >
+          +
+        </button>
       </div>
-      
     </>
   );
 }
