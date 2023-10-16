9dc6e312ea9c58722b24fdf3daf08a7490fa1bc8
Aaloklowanshi <lowanshiaalok@gmail.com>
2023-10-16 15:54:00 +0530

    added conditionally rendering for createnote

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -7,23 +7,57 @@ import Notes, { Note } from './Notes';
 import { useNavigate } from 'react-router-dom';
 
 export function HomePage() {
+  const [filteredNotes, setFilteredNotes] = useState([]); 
   const navigate = useNavigate();
+
+  const [notes, setNotes] = useState([]); // Initialize notes state
+
+  useEffect(() => {
+    // Fetch notes data and update the notes state
+    async function fetchNotes() {
+      try {
+        const response = await axios.get('http://localhost:8000/user/allnote', {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        });
+        setNotes(response.data.notes);
+      } catch (error) {
+        console.error('Error fetching notes:', error);
+      }
+    }
+
+    fetchNotes(); // Fetch notes when the component mounts
+  }, []);
+
+  
+  const isUserLoggedIn = localStorage.getItem('token')
+
   return (
     <>
-      <TopBar />
-      <Notes/>
+      <TopBar notes={notes} setFilteredNotes={setFilteredNotes} />
+      <Notes />
       <div className="homepage">
-
-            <button className="plus-icon"
+        {isUserLoggedIn ? (  // Conditional rendering of the "+" button
+          <button
+            className="plus-icon"
             onClick={() => {
-              navigate("/createnote")
-          }}
-            >   +
-            
-            </button>
-      
+              navigate('/createnote');
+            }}
+          >
+            +
+          </button>
+        ) : (
+          <button
+            className="plus-icon"
+            onClick={() => {
+              navigate('/login'); // Redirect to login if not logged in
+            }}
+          >
+            +
+          </button>
+        )}
       </div>
-      
     </>
   );
 }
